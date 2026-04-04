d7407c1b25704543d6ab998b00d5b00d94c48338
Murphy Sünnenwold <git@murphy.science>
2026-04-04 11:09:29 +0200

    add dependenciesInfo

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,11 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "org.pinpoint"
